b50aa91c301b92fd0222697e79358652dadfaadf
AlessandroVe <aveneziano536@gmail.com>
2021-06-21 09:03:35 +0200

    .

---
+++ Snack 3/js/script.js
@@ -0,0 +1,6 @@
+/* Snack 3
+Abbiamo sempre un array di squadre, con:
+nome
+punti fatti
+falli subiti
+Stampare in html (in forma tabellare!!! ) i dati relativi alle squadre evidenziando in giallo la riga corrispondente alla squadra con più falli subiti. */
